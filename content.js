b153a2b3ad75b1c248e60947d4f4d76f38d3603f
KunwarVSuryavanshi <virajkunwar98@gmail.com>
2022-02-23 00:04:51 +0530

    persistent color

--- content.js
+++ content.js
@@ -1,7 +1,24 @@
 chrome.runtime.onMessage.addListener(cont_func)
+console.log("Running content js")
+
+window.addEventListener('load', () => {
+  console.log("Content reloaded")
+  chrome.storage.sync.get('bg_color', (obj) => cont_func({property: 'bg_color'}))
+})
 
 function cont_func(request) {
-  let selectors = document.getElementsByTagName('body')
-
-  selectors[0].style['background-color'] = request.color.includes('#',0) ? request.color : `#${request.color}`
+  console.log("case",request)
+  switch(request.property)
+  {
+    case 'bg_color':
+      let selectors = document.getElementsByTagName('body')
+      chrome.storage.sync.get('bg_color', (obj) => {selectors[0].style['background-color'] = obj.bg_color; console.log(obj.bg_color)})
+      // selectors[0].style['background-color'] = request.value
+      break;
+    //case 'text_color':
+    default:
+      return
+  }
 }
+
+// let bg_func = chrome.storage.local.get('bg_color', () => cont_func({property: 'bg_color'}))
--- popup.js
+++ popup.js
@@ -1,10 +1,28 @@
-document.getElementById('color_code_input').addEventListener('change', getColorCode)
+console.log("Running pop up.js file")
 
-function getColorCode(e) {
+window.addEventListener("load", function() { 
+  console.log("window on load")
+  document.getElementById('color_code_input').addEventListener('change', getBgColorCode)
+
+  // chrome.storage.local.get('bg_color', (result) => {
+  //   document.getElementById('color_code_input').value = result.bg_color
+  //   console.log(result.bg_color)
+  // })
+});
+
+function getBgColorCode(e){
+  console.log("get bg color function")
+  if(e?.target?.value)
+  {
+    chrome.storage.sync.set({'bg_color': e.target.value}, () => getColorCode('bg_color'))
+  }
+}
+
+function getColorCode(location) {
   // document.getElementById('color_code_input').addEventListener('change', getColorCode)
-
   // let code = document.getElementById('color_code_input').value
+  console.log("Getcolrocode")
   chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-    chrome.tabs.sendMessage(tabs[0].id, {color: e.target.value})
+    chrome.tabs.sendMessage(tabs[0].id, {property: location})
   })
 }

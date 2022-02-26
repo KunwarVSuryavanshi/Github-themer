d3799bf900990f248805b0df5a9ff2859d21e53d
KunwarVSuryavanshi <virajkunwar98@gmail.com>
2022-02-26 20:42:41 +0530

    main dashboard + profile bg

--- content.js
+++ content.js
@@ -4,18 +4,36 @@ console.log("Running content js")
 window.addEventListener('load', () => {
   console.log("Content reloaded")
   chrome.storage.sync.get('bg_color', (obj) => cont_func({property: 'bg_color'}))
+  chrome.storage.sync.get('bg_color', (obj) => cont_func({property: 'a_color'}))
 })
+
+let selectors_bg = document.getElementsByTagName('body')
+let selectors_dashboard = document.getElementById('dashboard')
+let selector_sidePanel = document.getElementsByClassName('team-left-column')
+let selector_dash_main = document.getElementsByClassName('flex-auto col-md-8 col-lg-8 px-3 px-lg-5')
+let selector_side_main = document.getElementsByClassName('dashboard-sidebar')
+let selector_side_main_a = document.getElementsByClassName('dashboard-sidebar > div > div > ul')
 
 function cont_func(request) {
   console.log("case",request)
   switch(request.property)
   {
     case 'bg_color':
-      let selectors = document.getElementsByTagName('body')
-      chrome.storage.sync.get('bg_color', (obj) => {selectors[0].style['background-color'] = obj.bg_color; console.log(obj.bg_color)})
+      chrome.storage.sync.get('bg_color', (obj) => {
+        selectors_bg[0].style['background-color'] = obj.bg_color; 
+        selectors_dashboard.style['background-color'] = obj.bg_color;
+        selector_sidePanel[0].style['background-color'] = `${obj.bg_color} !important`
+        selector_sidePanel[1].style['background-color'] = `${obj.bg_color}`
+        selector_dash_main[0].style['background-color'] = `${obj.bg_color}`
+        selector_side_main[1].style['background-color'] = `${obj.bg_color}`
+      })
       // selectors[0].style['background-color'] = request.value
       break;
-    //case 'text_color':
+    case 'a_color': 
+      chrome.storage.sync.get('a_color', (obj) => {
+        console.log('OBJ---->',obj)
+        console.log(selector_side_main_a)
+      })
     default:
       return
   }
--- popup.js
+++ popup.js
@@ -2,7 +2,12 @@ console.log("Running pop up.js file")
 
 window.addEventListener("load", function() { 
   console.log("window on load")
-  document.getElementById('color_code_input').addEventListener('change', getBgColorCode)
+  document.getElementById('color_code_input_bg').addEventListener('change', (e) => {
+     chrome.storage.sync.set({'bg_color': e.target.value}, () => getColorCode('bg_color'))
+  })
+  document.getElementById('color_code_input_a').addEventListener('change', (e) => {
+    chrome.storage.sync.set({'a_color': e.target.value}, () => getColorCode('a_color'))
+  })
 
   // chrome.storage.local.get('bg_color', (result) => {
   //   document.getElementById('color_code_input').value = result.bg_color
@@ -10,18 +15,17 @@ window.addEventListener("load", function() {
   // })
 });
 
-function getBgColorCode(e){
-  console.log("get bg color function")
-  if(e?.target?.value)
-  {
-    chrome.storage.sync.set({'bg_color': e.target.value}, () => getColorCode('bg_color'))
-  }
-}
+// function getBgColorCode(e, region){
+//   console.log("get bg color function")
+//   if(e?.target?.value)
+//   {
+//     chrome.storage.sync.set({region: e.target.value}, () => getColorCode(region))
+//   }
+// }
 
 function getColorCode(location) {
   // document.getElementById('color_code_input').addEventListener('change', getColorCode)
   // let code = document.getElementById('color_code_input').value
-  console.log("Getcolrocode")
   chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
     chrome.tabs.sendMessage(tabs[0].id, {property: location})
   })
